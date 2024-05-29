cfeb56bf54c9ff2fc6b1904acc90f0485d028704
Jasur <rakhmatovjasur3@gmail.com>
2024-05-29 11:37:27 +0500

    main button

--- server/index.js
+++ server/index.js
@@ -11,21 +11,47 @@ const botStart = () => {
     if (text === "/start") {
       await bot.sendMessage(
         chatId,
-        "Jasur.inc platformasida bor kurslarni sotib olishingiz mumkin",{
-            reply_markup:{
-                keyboard:[
-                    [
-                        {
-                            text:"Kurslarni ko'rish",
-                            web_app:{
-                                url:"https://uz-online-shopbot.vercel.app"
-                            }
-                        }
-                    ]
-                ]
-            }
+        "Jasur.inc platformasida bor kurslarni sotib olishingiz mumkin",
+        {
+          reply_markup: {
+            keyboard: [
+              [
+                {
+                  text: "Kurslarni ko'rish",
+                  web_app: {
+                    url: "https://uz-online-shopbot.vercel.app",
+                  },
+                },
+              ],
+            ],
+          },
         }
       );
+    }
+    if (msg.web_app_data?.data) {
+      try {
+        const data = JSON.parse(msg.web_app_data?.data);
+        await bot.sendMessage(
+          chatId,
+          "Bizga ishonch bildirganingiz uchun rahmat, siz sotib olgan kurslar ro'yhati"
+        );
+        for (item of data) {
+            await bot.sendPhoto(chatId,item.Image)
+          await bot.sendMessage(chatId, `${item.title}-${item.quantity}x`);
+        }
+
+        await bot.sendMessage(
+          chatId,
+          `Umumiy narx: ${data
+            .reduce((a, c) => a + c.price * c.quantity, 0)
+            .toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}`
+        );
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 };
